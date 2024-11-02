f7792f04bff862705620601dc8aac502f3f8ce25
Shishira8 <shivashishira@gmail.com>
2024-11-02 11:54:51 -0700

    additional functions

--- engine.py
+++ engine.py
@@ -64,3 +64,17 @@ class Value:
         self.grad = 1
         for v in reversed(topo):
             v._backward()
+    
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    
